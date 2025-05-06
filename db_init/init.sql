c7effd37d94f7441e32c5953af8a5b7c11c57f40
Roman <nevsky0802@gmail.com>
2025-05-06 09:50:12 +0200

    it's just to elim data_bd

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,5 +1,10 @@
 CREATE DATABASE IF NOT EXISTS tickets CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
+CREATE USER IF NOT EXISTS 'usuari'@'%' IDENTIFIED BY 'paraula_de_pas';
+
+GRANT ALL PRIVILEGES ON tickets.* TO 'usuari'@'%';
+FLUSH PRIVILEGES;
+
 USE tickets;
 
 CREATE TABLE IF NOT EXISTS incidents (
